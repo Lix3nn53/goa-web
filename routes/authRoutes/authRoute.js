040d6254f7dea7507a227423f8096d5c71e8c85e
Lix3nn53 <lixakyildiz@gmail.com>
2020-04-21 05:40:39 +0300

    Fix refreshToken check to work with sessions, add session & sessions routes

--- routes/authRoutes/authRoute.js
+++ routes/authRoutes/authRoute.js
@@ -31,16 +31,59 @@ module.exports = (app) => {
     jwt.verify(refreshToken, keys.refreshTokenSecret, async (err, user) => {
       if (err) return res.sendStatus(403);
 
-      //remove refresh tokens that is not equal to the one in header
-      const filtered = user.sessions.filter(
+      //find session
+      const found = user.sessions.find(
         (session) => session.refreshToken === refreshToken
       );
-      user.sessions = filtered;
+      if (!found) return res.sendStatus(403); //refresh token is not valid for user
+
+      user.sessions = [found];
 
       await req.user.save();
-    });
 
-    res.send({});
+      res.send(user.sessions);
+    });
+  });
+
+  app.get("/api/session", authenticateAccessToken, (req, res) => {
+    const authHeader = req.headers["authorization"];
+    const refreshToken = authHeader && authHeader.split(" ")[1];
+    if (refreshToken == null) return res.sendStatus(401);
+
+    jwt.verify(refreshToken, keys.refreshTokenSecret, async (err, user) => {
+      if (err) return res.sendStatus(403);
+
+      //find session
+      const found = user.sessions.find(
+        (session) => session.refreshToken === refreshToken
+      );
+      if (!found) return res.sendStatus(403); //refresh token is not valid for user
+
+      res.send(found);
+    });
+  });
+
+  app.get("/api/sessions", authenticateAccessToken, (req, res) => {
+    const authHeader = req.headers["authorization"];
+    const refreshToken = authHeader && authHeader.split(" ")[1];
+    if (refreshToken == null) return res.sendStatus(401);
+
+    jwt.verify(refreshToken, keys.refreshTokenSecret, async (err, user) => {
+      if (err) return res.sendStatus(403);
+
+      //find session
+      const found = user.sessions.find(
+        (session) => session.refreshToken === refreshToken
+      );
+      if (!found) return res.sendStatus(403); //refresh token is not valid for user
+
+      //remove refresh token that is equal to the one in header
+      const filtered = user.sessions.filter(
+        (session) => session.refreshToken !== refreshToken
+      );
+
+      res.json({ current: found, other: filtered });
+    });
   });
 
   app.post("/refreshAccessToken", (req, res) => {
@@ -51,8 +94,11 @@ module.exports = (app) => {
     jwt.verify(refreshToken, keys.refreshTokenSecret, (err, user) => {
       if (err) return res.sendStatus(403);
 
-      const includes = user.refreshTokens.includes(refreshToken);
-      if (!includes) return res.sendStatus(403); //refresh token is not valid for user
+      //find session
+      const found = user.sessions.find(
+        (session) => session.refreshToken === refreshToken
+      );
+      if (!found) return res.sendStatus(403); //refresh token is not valid for user
 
       const accessToken = generateAccessToken({ name: user.name });
       res.json({ accessToken: accessToken });
